678eb53de31dd25b1b70cd2957c78a76dc1f6d5b
Ilnur <ilnrzakirov@gmail.com>
2022-02-03 22:20:28 +0300

    create buildins hub

--- includes/minishell.h
+++ includes/minishell.h
@@ -80,5 +80,5 @@ int         ft_echo(char **arg);
 int     	ft_strcmp(const char *s1, const char *s2);
 int         ft_cd(t_data *data);
 void        ft_exit(t_lst *cmd, t_data *data);
-void        buildins_hub(t_lst *cmd, t_data *data)
+void        buildins_hub(t_lst *cmd, t_data *data);
 #endif
