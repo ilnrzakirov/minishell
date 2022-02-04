ea482e7bc5f0a1d982e114942b1ab2dc11cfc551
Ilnur <ilnrzakirov@gmail.com>
2022-02-04 22:32:08 +0300

    run ok

--- src/exe/buildins_hub.c
+++ src/exe/buildins_hub.c
@@ -3,7 +3,7 @@
 void    buildins_hub(t_lst *cmd, t_data *data)
 {
     if (ft_strstr(cmd->cmd[0], "cd"))
-        ft_cd(data);
+        ft_cd(cmd);
     else if (ft_strstr(cmd->cmd[0], "echo"))
         ft_echo(cmd->cmd);
     else if (ft_strstr(cmd->cmd[0], "exit"))
