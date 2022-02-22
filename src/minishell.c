19d9d987b3a56794ea6e1f11fe8b962fcc3489c1
bcarlee <bcarlee@student.21-school.ru>
2022-02-22 14:20:14 +0300

    add shell lvl up

--- src/minishell.c
+++ src/minishell.c
@@ -12,6 +12,36 @@
 
 #include "../includes/minishell.h"
 
+char	*get_value(char *key)
+{
+	t_env *temp;
+
+	temp = g_data->env;
+	while (temp && temp->key)
+	{
+		if (ft_strnstr(temp->key, key, ft_strlen(key)))
+			return (ft_strdup(temp->value));
+		temp = temp->next;
+	}
+	return (0);
+}
+
+void	replace_shell_lvl(void){
+	t_env	*temp;
+	char 	*key;
+	char 	*value;
+	int 	old_sh_lvl;
+
+	temp = g_data->env;
+	key = ft_strdup("SHLVL=");
+	value = get_value(key);
+	old_sh_lvl = ft_atoi(value);
+	old_sh_lvl++;
+	free(value);
+	value = ft_itoa(old_sh_lvl);
+	replace_value_envp(key, value);
+}
+
 t_env *init_env(t_data *data, char **env)
 {
     int     i;
@@ -45,8 +75,9 @@ int	main(int i, char **argv, char **env)
 
 	if (i > 1)
 		return (print_error("No such file or directory\n", 2));
-	envp = init_env(&data, env);
+    envp = init_env(&data, env);
     g_data = &data;
+	replace_shell_lvl();
     data.exit_code = 0;
 	while (1)
 	{
