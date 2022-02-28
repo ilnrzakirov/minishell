18d7fc37dc19d42af6dec4da731586d501f5b719
sshera <tanya.myau@mail.ru>
2022-02-28 15:00:38 +0300

    norma exe

--- src/exe/buildins_hub.c
+++ src/exe/buildins_hub.c
@@ -3,47 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   buildins_hub.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bcarlee <bcarlee@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sshera <sshera@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 09:40:05 by bcarlee           #+#    #+#             */
-/*   Updated: 2022/02/20 09:40:07 by bcarlee          ###   ########.fr       */
+/*   Updated: 2022/02/28 14:49:04 by sshera           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int    buildins_hub_parent(t_lst *cmd)
+int	buildins_hub_parent(t_lst *cmd)
 {
 	if (g_data ->check_path == 1)
 		return (0);
-	if (ft_strstr(cmd->cmd[0], "export")) {
+	if (ft_strstr(cmd->cmd[0], "export"))
+	{
 		ft_export(cmd, 0);
 		return (1);
 	}
-	else if (ft_strstr(cmd->cmd[0], "unset")) {
+	else if (ft_strstr(cmd->cmd[0], "unset"))
+	{
 		ft_unset(cmd);
 		return (1);
 	}
-	else if (ft_strstr(cmd->cmd[0], "cd")) {
+	else if (ft_strstr(cmd->cmd[0], "cd"))
+	{
 		ft_cd(cmd);
 		return (1);
 	}
 	else if (ft_strstr(cmd->cmd[0], "exit"))
-		ft_exit(cmd ,g_data);
+		ft_exit(cmd, g_data);
 	return (0);
 }
 
-void    buildins_hub(t_lst *cmd, t_data *data)
+void	buildins_hub(t_lst *cmd, t_data *data)
 {
 	if (g_data ->check_path == 1)
 		exit(EXIT_FAILURE);
-    if (ft_strstr(cmd->cmd[0], "cd"))
-        ft_cd(cmd);
-    else if (ft_strstr(cmd->cmd[0], "echo"))
-        ft_echo(cmd->cmd);
-    else if (ft_strstr(cmd->cmd[0], "env"))
-        ft_env(data->env);
-    else if (ft_strstr(cmd->cmd[0], "pwd"))
+	if (ft_strstr(cmd->cmd[0], "cd"))
+		ft_cd(cmd);
+	else if (ft_strstr(cmd->cmd[0], "echo"))
+		ft_echo(cmd->cmd);
+	else if (ft_strstr(cmd->cmd[0], "env"))
+		ft_env(data->env);
+	else if (ft_strstr(cmd->cmd[0], "pwd"))
 	{
 		if (ft_pwd(0))
 			exit(EXIT_SUCCESS);
--- src/exe/utils.c
+++ src/exe/utils.c
@@ -3,94 +3,96 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bcarlee <bcarlee@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sshera <sshera@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 13:53:42 by bcarlee           #+#    #+#             */
-/*   Updated: 2022/02/01 13:53:43 by bcarlee          ###   ########.fr       */
+/*   Updated: 2022/02/28 14:47:36 by sshera           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int ft_env_lstsize(t_env *e)
+int	ft_env_lstsize(t_env *e)
 {
-    int	    count;
-    t_env   *env;
+	int		count;
+	t_env	*env;
 
-    env = e;
-    count = 0;
-    while (env)
-    {
-        env = env->next;
-        count++;
-    }
-    return (count);
+	env = e;
+	count = 0;
+	while (env)
+	{
+		env = env->next;
+		count++;
+	}
+	return (count);
 }
 
-char **get_env(t_data *data)
+char	**get_env(t_data *data)
 {
-    int     i;
-    t_env   *env;
-    char    **res;
+	int		i;
+	t_env	*env;
+	char	**res;
 
-    env = data->env;
-    i = 0;
-    res = malloc(sizeof(char*) * ft_env_lstsize(env) + 1);
-    while(env)
-    {
-        res[i++] = ft_strjoin(ft_strdup(env->key), env->value);
-        env = env->next;
-    }
+	env = data->env;
+	i = 0;
+	res = malloc(sizeof(char *) * ft_env_lstsize(env) + 1);
+	while (env)
+	{
+		res[i++] = ft_strjoin(ft_strdup(env->key), env->value);
+		env = env->next;
+	}
 	res[i] = NULL;
-    return (res);
+	return (res);
 }
 
-void clear_struct()
+void	clear_struct(void)
 {
-	int i;
-	t_lst *fr;
+	int		i;
+	t_lst	*fr;
+
 	while (g_data->cmd)
 	{
-			i = -1;
-			fr = g_data->cmd;
-			if (g_data->cmd->cmd) {
-				while (g_data->cmd->cmd[++i])
-					free(g_data->cmd->cmd[i]);
-				free(g_data->cmd->cmd);
-			}
-			if (g_data->cmd->filename)
-				free(g_data->cmd->filename);
-			g_data->cmd = g_data->cmd->next;
-			free(fr);
+		i = -1;
+		fr = g_data->cmd;
+		if (g_data->cmd->cmd)
+		{
+			while (g_data->cmd->cmd[++i])
+				free(g_data->cmd->cmd[i]);
+			free(g_data->cmd->cmd);
 		}
+		if (g_data->cmd->filename)
+			free(g_data->cmd->filename);
+		g_data->cmd = g_data->cmd->next;
+		free(fr);
+	}
 	g_data->cmd = NULL;
 }
 
-char *ft_find_path(char *str, int i)
+char	*ft_find_path(char *str, int i)
 {
-    char    *cmd;
-    char    *tmp;
-    char    **path;
+	char	*cmd;
+	char	*tmp;
+	char	**path;
 
-    cmd = ft_strjoin(ft_strdup("/"), str);
-    path = ft_split(getenv("PATH="), ':');
-    while(path[i])
-    {
-        tmp = ft_strjoin(ft_strdup(path[i]), cmd);
-        if (access(tmp, F_OK))
-        {
-            free(tmp);
-            tmp = NULL;
-        }
-        else
-        {
-            free(cmd);
-            clear_arr(path);
-            return (tmp);
-        }
-        i++;
-    }
-    clear_arr(path);
-    free(cmd);
-    return (NULL);
+	cmd = ft_strjoin(ft_strdup("/"), str);
+	path = ft_split(getenv("PATH="), ':');
+	while (path[i])
+	{
+		tmp = ft_strjoin(ft_strdup(path[i]), cmd);
+		if (access(tmp, F_OK))
+		{
+			free(tmp);
+			tmp = NULL;
+		}
+		else
+		{
+			free(cmd);
+			clear_arr(path);
+			return (tmp);
+		}
+		i++;
+	}
+	clear_arr(path);
+	free(cmd);
+	return (NULL);
 }
